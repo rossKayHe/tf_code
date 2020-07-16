65bb180676f59aedf0f577c82d7ebb3cb30e0522
rossKayHe <rossKayHe@users.noreply.github.com>
2020-07-16 07:43:45 -0700

    two web servers and elastic IP

--- prod.tf
+++ prod.tf
@@ -1,4 +1,4 @@
-provider "aws {
+provider "aws" {
     profile =  "default"
     region  = "us-west-2"
 }
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "prod_tf_sample" {
     acl    = "private"
 }
 
-resouce "aws_default_vpc" "default" {}
+resource "aws_default_vpc" "default" {}
 
 resource "aws_security_group" "prod_web"  {
     name        = "prod_web"
@@ -37,14 +37,14 @@ resource "aws_security_group" "prod_web"  {
         "Terraform" : "true"
     }
 }
-resource "aws_instance" " prod_web" {
+resource "aws_instance" "prod_web" {
     count = 2
 
     ami           = "ami-01ebd27ddd89835f4"
-    instance_type = t2.nano
+    instance_type = "t2.nano"
 
     vpc_security_group_ids = [
-        aws_security_group.web.id
+        aws_security_group.prod_web.id
     ]
 
     tags = {
@@ -52,9 +52,9 @@ resource "aws_instance" " prod_web" {
     }
 } 
 
-resource "aws_eip_asociation" "prod_web" {
-    instance_id = aws_instance.prod_web.0.id
-    allocation_id = aws.eip.prod_web.id
+resource "aws_eip_association" "prod_web" {
+    instance_id   = aws_instance.prod_web.0.id
+    allocation_id = aws_eip.prod_web.id
 }
 
 resource "aws_eip" "prod_web" {
